bf748a10e4dbf3f6224b952202c42f8dc69630c8
Joan Gerard <joan.gerard@outlook.com>
2025-02-25 21:03:38 +0100

    create Hero

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Navbar from "@/components/Navbar.vue"
+import Hero from "@/components/Hero.vue"
 </script>
 
 <template>
     <Navbar />
+    <Hero />
 </template>
 
 <style scoped></style>
