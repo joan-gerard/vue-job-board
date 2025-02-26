e6bf44b99fe49b42667c253326e80d61076b968e
Joan Gerard <joan.gerard@outlook.com>
2025-02-26 14:37:51 +0100

    truncate job desc and add toggle to show full desc

--- src/components/JobListing.vue
+++ src/components/JobListing.vue
@@ -1,10 +1,22 @@
 <script setup lang="ts">
-import { defineProps } from "vue"
+import { defineProps, computed, ref } from "vue"
 
-defineProps({
+const props = defineProps({
     job: Object
 })
+const showFullDesc = ref(false)
 
+const toggleFullDesc = () => {
+    showFullDesc.value = !showFullDesc.value
+}
+
+const truncateDescription = computed(() => {
+    let description = props.job.description
+    if (!showFullDesc.value) {
+        description = description.substring(0, 90) + '...'
+    }
+    return description
+})
 </script>
 
 <template>
@@ -16,7 +28,13 @@ defineProps({
             </div>
 
             <div class="mb-5">
-                {{ job.description }}
+                <div>
+
+                    {{ truncateDescription }}
+                </div>
+                <button class="text-green-500 hover:text-green-600 mb-5" @click="toggleFullDesc">
+                    {{ showFullDesc ? "Less" : "More" }}
+                </button>
             </div>
 
             <h3 class="text-green-500 mb-2">{{ job.salary }} / Year</h3>
@@ -25,7 +43,7 @@ defineProps({
 
             <div class="flex flex-col lg:flex-row justify-between mb-4">
                 <div class="text-orange-700 mb-3">
-                    <i class="fa-solid fa-location-dot text-lg"></i>
+                    <i class="pi pi-map-marker text-orange-700"></i>
                     {{ job.location }}
                 </div>
                 <a v-bind:href="'/job/' + job.id"
