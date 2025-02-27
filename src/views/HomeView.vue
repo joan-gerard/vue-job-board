9bec46e13064591112044a1344613eaadebb5d14
Joan Gerard <joan.gerard@outlook.com>
2025-02-27 20:31:31 +0100

    test error fix

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import Hero from "@/components/Hero.vue"
 import HomeCards from "@/components/HomeCards.vue"
 import JobListings from "@/components/JobListings.vue"
+import Hero from '@/components/Hero.vue';
 </script>
 
 <template>
