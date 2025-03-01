ce673162792209f7695b0762c4e489011cf43b55
Joan Gerard <joan.gerard@outlook.com>
2025-03-01 19:37:39 +0100

    fetch server data in single job page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,33 +1,38 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
-import JobsView from '@/views/JobsView.vue'
-import NotFoundView from '@/views/NotFoundView.vue'
-import JobView from '@/views/JobView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "@/views/HomeView.vue";
+import JobsView from "@/views/JobsView.vue";
+import NotFoundView from "@/views/NotFoundView.vue";
+import JobView from "@/views/JobView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'Home',
+      path: "/",
+      name: "Home",
       component: HomeView,
     },
     {
-      path: '/jobs',
-      name: 'Jobs',
+      path: "/jobs",
+      name: "Jobs",
       component: JobsView,
     },
+    // {
+    //   path: '/jobs/add',
+    //   name: 'Add Jobs',
+    //   component: JobsView,
+    // },
     {
-      path: '/jobs/:id',
-      name: 'Job',
+      path: "/jobs/:id",
+      name: "Job",
       component: JobView,
     },
     {
-      path: '/:catchAll(.*)',
-      name: 'Not-found',
+      path: "/:catchAll(.*)",
+      name: "Not-found",
       component: NotFoundView,
     },
   ],
-})
+});
 
-export default router
+export default router;
