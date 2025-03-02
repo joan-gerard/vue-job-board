9579f3ba596e7222764b02628a34b3d7e13a27fd
Joan Gerard <joan.gerard@outlook.com>
2025-03-02 15:10:21 +0100

    add Edit Job functionality

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,7 @@ import JobsView from "@/views/JobsView.vue";
 import NotFoundView from "@/views/NotFoundView.vue";
 import JobView from "@/views/JobView.vue";
 import AddJobView from "@/views/AddJobView.vue";
+import EditJobView from "@/views/EditJobView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -22,6 +23,11 @@ const router = createRouter({
       path: "/jobs/add",
       name: "add-jobs",
       component: AddJobView,
+    },
+    {
+      path: "/jobs/edit/:id",
+      name: "edit-jobs",
+      component: EditJobView,
     },
     {
       path: "/jobs/:id",
